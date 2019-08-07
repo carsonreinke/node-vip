bdba63245eb2a9b715485846c1038ec7ab76964c
Carson Reinke <carson@reinke.co>
2019-08-07 13:08:23 -0400

    Added some packages to ignore

--- score-dependencies.js
+++ score-dependencies.js
@@ -1,10 +1,53 @@
 const dependencies = require('./dependencies');
 
+const naughtyPackages = [
+    'lower-case-nodered',
+    'readtest',
+    'statusnode',
+    'playnode',
+    'languenode',
+    'menunode',
+    'callnode',
+    'skillstatenode',
+    'callodig',
+    'allnodes',
+    'requestnodelecomte',
+    'writenodelecomte',
+    'routingnodelecomte',
+    'drivenode',
+    'endnode',
+    'executenode',
+    'messagenodelecomte',
+    'questionmessage',
+    'nodemessagelecomte',
+    'languenodedeux',
+    'questionlecomte',
+    'requestusser',
+    'questionfirstinfo',
+    'questiongetsetting',
+    'readtesta',
+    'responselecomte',
+    'domeconect',
+    'smsodigo',
+    'emailodigo',
+    'receiveodigomessage',
+    'carousselodigo',
+    'youtubeodigo',
+    'checkboxodigo',
+    'placemarkerodigo',
+    'positionodigo',
+    'positionnameodigo',
+    'adressodigo',
+    'nearodigo',
+    'node-odigo-first',
+    'node-red-contrib-odigo'
+];
+
 const scoreDependencies = async(path, pkgs) => {
     const scores = {};
     const depth = 2.0 ** -(path.length - 1);
 
-    //console.debug(`Scoring dependencies from ${path.join(' > ')}`);
+    console.debug(`Scoring dependencies from ${path.join(' > ')}`);
 
     for(let pkg of pkgs) {
         //Check for circular dependencies
@@ -13,7 +56,7 @@ const scoreDependencies = async(path, pkgs) => {
         }
 
         //Ignore some special packages used for dependency testing
-        if(pkg.match(/^strongcanary/) || pkg.match(/^npm-cycle/) || pkg.match(/^npm-circdep-test/)) {
+        if(naughtyPackages.includes(pkg) || pkg.match(/^strongcanary/) || pkg.match(/^npm-cycle/) || pkg.match(/^npm-circdep-test/)) {
             continue;
         }
 
