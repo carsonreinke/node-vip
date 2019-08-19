08bf4ab10f9f5eaa4f468d8c81a9dd9d737ca854
Carson Reinke <carson@reinke.co>
2019-08-18 22:26:08 -0400

    Correct report data Added Google chart table

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import {GoogleCharts} from 'google-charts';
 const report = require('../report.json');
 
-GoogleCharts.load(() => {
+const drawChart = () => {
     const data = new GoogleCharts.api.visualization.DataTable();
     data.addColumn('string', 'Package');
     data.addColumn('number', 'Score');
-    report.entries.forEach(entry => {
-        data.addRow([entry.name, entry.score);
+    report.entries.slice(0, 9).forEach(entry => {
+        data.addRow([entry.name, entry.score]);
     });
-    data.addRow(['Others', report.others.score]);
+    //data.addRow(['Others', report.others.score]);
 
     const chart = new GoogleCharts.api.visualization.PieChart(document.getElementById('chart'));
     chart.draw(data);
-});
+};
+
+const drawTable = () => {
+    const data = new GoogleCharts.api.visualization.DataTable();
+    data.addColumn('string', 'Package');
+    data.addColumn('number', 'Score');
+    data.addColumn('number', 'Impact');
+    report.entries.forEach(entry => {
+        data.addRow([entry.name, entry.score, entry.impact]);
+    });
+
+    const chart = new GoogleCharts.api.visualization.Table(document.getElementById('table'));
+    chart.draw(data);
+};
+
+GoogleCharts.load(() => {
+    drawChart();
+    drawTable();
+}, {packages: ['table']});
